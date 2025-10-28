e5a1581cacd27cb77cda1b202c88ae3cfa5c6a11
DekchaiHeart <110713141+DekchaiHeart@users.noreply.github.com>
2025-10-28 21:08:06 +0700

    [pbi-01] first commit

---
+++ sit-event-web/src/features/event_management/components/CreateUpdate_Event.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref('Hello Vue 3')
+</script>
+
+<template>
+  <div class="container">Content here</div>
+  <h1>{{ message }}</h1>
+</template>
+
+<style scoped>
+.container {
+  /* styles */
+}
+</style>
